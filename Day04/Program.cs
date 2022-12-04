53f5c0cebcbaaff7edddee346e3daf8d141543ad
Jakub Findura <findurajakub@gmail.com>
2022-12-04 12:10:25 +0100

    Day 04

---
+++ Day04/Program.cs
@@ -0,0 +1,37 @@
+﻿void First()
+{
+    var contained = 0;
+    foreach (var line in File.ReadLines(@"input.txt"))
+    {
+        var elfOne = line.Split(",")[0].Split("-").Select(s => int.Parse(s)).ToArray();
+        var elfTwo = line.Split(",")[1].Split("-").Select(s => int.Parse(s)).ToArray();
+
+        if (elfOne[0] <= elfTwo[0] && elfOne[1] >= elfTwo[1])
+            contained++;
+        else if (elfOne[1] <= elfTwo[1] && elfOne[0] >= elfTwo[0])
+            contained++;
+    }
+
+    Console.WriteLine($"Contained: {contained}");
+}
+
+void Second()
+{
+    var overlapping = 0;
+    foreach (var line in File.ReadLines(@"input.txt"))
+    {
+        var elfOne = line.Split(",")[0].Split("-").Select(s => int.Parse(s)).ToArray();
+        var elfTwo = line.Split(",")[1].Split("-").Select(s => int.Parse(s)).ToArray();
+
+        if (elfOne[0] <= elfTwo[0] && elfTwo[0] <= elfOne[1])
+            overlapping++;
+        else if (elfTwo[0] <= elfOne[0] && elfOne[0] <= elfTwo[1])
+            overlapping++;
+    }
+
+    Console.WriteLine($"Overlapped: {overlapping}");
+}
+
+First();
+
+Second();
