e5df81e58858cfbbd3cd9066b82895de14a294d0
jcubes <findurajakub@gmail.com>
2022-12-06 15:36:56 +0100

    Day 06

---
+++ Day06/Program.cs
@@ -0,0 +1,31 @@
+﻿void First()
+{
+    var line = File.ReadAllText(@"input.txt");
+    for (int i = 0; i < line.Length - 4; i++)
+    {
+        if (line.Skip(i).Take(4).Distinct().Count() == 4)
+        {
+            Console.WriteLine($"start-of-packet: {i + 4}");
+            break;
+        }
+    }
+
+}
+
+void Second()
+{
+    var line = File.ReadAllText(@"input.txt");
+    for (int i = 0; i < line.Length - 14; i++)
+    {
+        if (line.Skip(i).Take(14).Distinct().Count() == 14)
+        {
+            Console.WriteLine($"start-of-message: {i + 14}");
+            break;
+        }
+    }
+
+}
+
+First();
+
+Second();
