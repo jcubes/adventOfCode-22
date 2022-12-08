208a8b6a9a97d0359aab11d48f7893fef87ba53c
jcubes <findurajakub@gmail.com>
2022-12-08 21:44:05 +0100

    Day 07

---
+++ Day07/Dir.cs
@@ -0,0 +1,46 @@
+﻿class Dir
+{
+    public string name;
+    public Dictionary<string, DirFile> files;
+    public Dictionary<string, Dir> sub;
+    public Dir parent;
+
+    public Dir()
+    {
+        files = new Dictionary<string, DirFile>();
+        sub = new Dictionary<string, Dir>();
+    }
+
+    public int Size(ref int c)
+    {
+        var fileSizeSum = files.Values.Select(f => f.size).Sum();
+        var dirSizeSum = 0;
+
+        foreach (var item in sub.Values)
+            dirSizeSum += item.Size(ref c);
+
+        if (fileSizeSum + dirSizeSum <= 100000)
+            c += fileSizeSum + dirSizeSum;
+        return fileSizeSum + dirSizeSum;
+    }
+
+    public int Size2(int toFree, ref int minToFree)
+    {
+        var fileSizeSum = files.Values.Select(f => f.size).Sum();
+        var dirSizeSum = 0;
+
+        foreach (var item in sub.Values)
+            dirSizeSum += item.Size2(toFree, ref minToFree);
+
+        if (fileSizeSum + dirSizeSum > toFree && fileSizeSum + dirSizeSum < minToFree)
+            minToFree = fileSizeSum + dirSizeSum;
+
+        return fileSizeSum + dirSizeSum;
+    }
+}
+
+class DirFile
+{
+    public string name;
+    public int size;
+}
---
+++ Day07/Program.cs
@@ -0,0 +1,126 @@
+﻿void First()
+{
+    var root = new Dir { name = "/" };
+    var cd = root;
+
+    var lines = File.ReadLines(@"input.txt").ToArray();
+    var i = 0;
+
+    while (i < lines.Length)
+    {
+        var line = lines[i];
+
+        if (line.StartsWith("$"))
+        {
+            var cmd = line.Split(" ");
+            if (cmd[1] == "cd")
+            {
+                if (cmd[2] == "/")
+                    cd = root;
+                else if (cmd[2] == "..")
+                    cd = cd.parent;
+                else
+                {
+                    if (!cd.sub.ContainsKey(cmd[2]))
+                        cd.sub.Add(cmd[2], new Dir { name = cmd[2], parent = cd });
+
+                    cd = cd.sub[cmd[2]];
+                }
+            }
+            if (cmd[1] == "ls")
+            {
+                i++;
+                while (i < lines.Length && !lines[i].StartsWith("$"))
+                {
+                    var sLine = lines[i].Split();
+                    if (sLine[0] == "dir" && !cd.sub.ContainsKey(sLine[1]))
+                        cd.sub.Add(sLine[1], new Dir { name = sLine[1], parent = cd });
+                    else
+                        cd.files.Add(sLine[1], new DirFile { name = sLine[1], size = int.Parse(sLine[0]) });
+                    i++;
+                }
+                if (i < lines.Length)
+                    i--;
+            }
+        }
+
+        i++;
+    }
+
+    int c = 0;
+    root.Size(ref c);
+
+    Console.WriteLine($"Result: {c}");
+}
+
+void Second()
+{
+    var root = new Dir { name = "/" };
+    var cd = root;
+
+    var lines = File.ReadLines(@"input.txt").ToArray();
+    var i = 0;
+
+    while (i < lines.Length)
+    {
+        var line = lines[i];
+
+        if (line.StartsWith("$"))
+        {
+            var cmd = line.Split(" ");
+            if (cmd[1] == "cd")
+            {
+                if (cmd[2] == "/")
+                    cd = root;
+                else if (cmd[2] == "..")
+                    cd = cd.parent;
+                else
+                {
+                    if (!cd.sub.ContainsKey(cmd[2]))
+                        cd.sub.Add(cmd[2], new Dir { name = cmd[2], parent = cd });
+
+                    cd = cd.sub[cmd[2]];
+                }
+            }
+            if (cmd[1] == "ls")
+            {
+                i++;
+                while (i < lines.Length && !lines[i].StartsWith("$"))
+                {
+                    var sLine = lines[i].Split();
+                    if (sLine[0] == "dir" && !cd.sub.ContainsKey(sLine[1]))
+                        cd.sub.Add(sLine[1], new Dir { name = sLine[1], parent = cd });
+                    else
+                        cd.files.Add(sLine[1], new DirFile { name = sLine[1], size = int.Parse(sLine[0]) });
+                    i++;
+                }
+                if (i < lines.Length)
+                    i--;
+            }
+        }
+
+        i++;
+    }
+
+    int c = 0;
+    var x = root.Size(ref c);
+
+    int max = 70000000;
+    int needed = 30000000;
+    int free = max - x;
+
+    var toFree = needed - free;
+    var minToFree = x;
+
+    root.Size2(toFree, ref minToFree);
+
+    Console.WriteLine($"Total: {max}");
+    Console.WriteLine($"Needed: {needed}");
+    Console.WriteLine($"Free: {free}");
+    Console.WriteLine($"toFree: {toFree}");
+    Console.WriteLine($"Result: {minToFree}");
+}
+
+First();
+
+Second();
